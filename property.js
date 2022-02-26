14878ceca9834b79dbcda6f6a1c9360396222c9e
Farhan910 <farhanfaruk221@gmail.comm>
2022-02-26 13:55:37 +0600

    final

--- property.js
+++ property.js
@@ -1,17 +1,17 @@
-// const bottle = { name:'RFL', price :60, hold: 'water'}
-// const keys = Object.keys(bottle)
-// console.log(keys);
-// const values = Object.values(bottle)
-// console.log(values);
-// const pairs = Object.entries(bottle)
-// console.log(pairs);
-// Object.seal(bottle)
-// delete bottle.hold;
-// console.log(bottle);
+const bottle = { name:'RFL', price :60, hold: 'water'}
+const keys = Object.keys(bottle)
+console.log(keys);
+const values = Object.values(bottle)
+console.log(values);
+const pairs = Object.entries(bottle)
+console.log(pairs);
+Object.seal(bottle)
+delete bottle.hold;
+console.log(bottle);
 
-// for(const prop in bottle){
-//     console.log ( prop ,bottle[prop]);
-// }
+for(const prop in bottle){
+    console.log ( prop ,bottle[prop]);
+}
 
 const obj ={a:1, b:7, c:3, length:2};
 console.log(Object.keys(obj).length);
